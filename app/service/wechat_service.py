a7621ca67700d8c6e16e5e9cea1392a168760c70
yeoshine <ydjjing@163.com>
2017-09-20 23:40:13 +0800

    update

--- app/service/wechat_service.py
+++ app/service/wechat_service.py
@@ -12,8 +12,13 @@ class WechatService:
     def message(data):
         msg = parse_message(data)
         if msg.type == 'text':
-            app.logger.warning(u'FromUserName: {FromUserName}, ' .format(FromUserName=msg.content))
-            reply=TextReply(content='text reply', message=msg)
+            app.logger.warning(
+                u'FromUserName: {FromUserName}, CreateTime: {CreateTime}, MsgId: {MsgId}, Content：{Content}'.format(
+                    FromUserName=msg.source,
+                    CreateTime=msg.create_time,
+                    MsgId=msg.id,
+                    Content=msg.content))
+            reply = TextReply(content='此功能正在开发中……', message=msg)
         else:
-            reply=create_reply('Sorry, can not handle this for now', msg)
+            reply = create_reply('此功能正在开发中……', msg)
         return reply.render()
