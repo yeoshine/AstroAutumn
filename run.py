29f85c1d8c9d213dc6c7637cc2e30eb758f637a5
yeoshine <ydjjing@163.com>
2017-10-06 22:38:53 +0800

    run

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ from app.routes import routes
 
 if __name__ == "__main__":
     app.debug = app.config['DEBUG']
-    app.run('0.0.0.0', debug=False, port=8443, ssl_context='adhoc')
+    app.run('0.0.0.0', debug=False, port=443, ssl_context='adhoc')
     # app.run('0.0.0.0', debug=True)
